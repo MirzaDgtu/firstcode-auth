f5f6e595e00a06357be0d3314ed652e81332903c
MirzaDgtu <mirzyali@mail.ru>
2024-12-11 15:02:10 +0300

    add user model

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -2,9 +2,13 @@ package auth
 
 import (
 	"context"
+	"errors"
+	"firstcode-auth/internal/grpc/auth"
 
-	ssov1 "github.com/MirzaDgtu/firstcode_protos"
+	ssov1 "github.com/MirzaDgtu/firstcode_protos/gen/go/sso"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type serverAPI struct {
@@ -37,12 +41,51 @@ func (s *serverAPI) Login(
 	ctx context.Context,
 	in *ssov1.LoginRequest,
 ) (*ssov1.LoginResponse, error) {
-	// TODO
+	if in.Email == "" {
+		return nil, status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	if in.Password == "" {
+		return nil, status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	if in.GetAppId() == 0 {
+		return nil, status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	token, err := s.auth.Login(ctx, in.GetEmail(), in.GetPassword(), int(in.GetAppId()))
+	if err != nil {
+		if errors.Is(err, auth.ErrInvalidCredentials) {
+			return nil, status.Error(codes.InvalidArgument, "invalid email or password")
+		}
+
+		return nil, status.Error(codes.Internal, "failed to login")
+	}
+
+	return &ssov1.LoginResponse{Token: token}, nil
 }
 
 func (s *serverAPI) Register(
 	ctx context.Context,
 	in *ssov1.RegisterRequest,
-) (*ssov1.RegisterResponse, error) {
-	// TODO
+) (*ssov1.RegisterResponce, error) {
+	if in.Email == "" {
+		return nil, status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	if in.Password == "" {
+		return nil, status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	uid, err := s.auth.RegisterNewUser(ctx, in.GetEmail(), in.GetPassword(), in.GetFirstName(),
+		in.GetName(), in.GetLastName(), in.GetPhone(), in.GetSex())
+	if err != nil {
+		if errors.Is(err, storage.ErrUserExists) {
+			return nil, status.Error(codes.AlreadyExists, "user already exists")
+		}
+
+		return nil, status.Error(codes.Internal, "failed to register user")
+	}
+
+	return &ssov1.RegisterResponce{UserId: uid}, nil
 }
