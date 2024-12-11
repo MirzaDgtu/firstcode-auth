b233ce8c600865da5d7e2fdcbe63155761221135
MirzaDgtu <mirzyali@mail.ru>
2024-12-12 02:35:09 +0700

    add migrations

---
+++ migrations/1_init.up.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    id           INTEGER PRIMARY KEY,
+    email        TEXT    NOT NULL UNIQUE,
+    pass_hash    BLOB    NOT NULL,
+    first_name   VARCHAR(100) NOT NULL,
+    name VARCHAR(40) NOT NULL,
+    last_name VARCHAR(130) NULL,
+    phone VARCHAR(20) NULL,
+    sex VARCHAR(3) NULL
+);
+CREATE INDEX IF NOT EXISTS idx_email ON users (email);
+
+CREATE TABLE IF NOT EXISTS apps
+(
+    id     INTEGER PRIMARY KEY,
+    name   TEXT NOT NULL UNIQUE,
+    secret TEXT NOT NULL UNIQUE
+);
